096a8547b16a75f60da6918f32db90d5aec5a739
JAbrams204 <94541938+JAbrams204@users.noreply.github.com>
2022-10-18 20:41:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+let deckId
+let computerScore = 0
+let myScore = 0
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
+
+function handleClick() {
+          //"https://deckofcardsapi.com/api/deck/new/shuffle/" 
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(response => response.json())
+        .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+            deckId = data.deck_id
+            console.log(deckId)
+
+            drawCardBtn.disabled = false
+
+          //   if (data.remaining === 0) {
+          //     computerScore === 0
+          //     myScore === 0
+          // }
+
+        })
+
+}
+
+newDeckBtn.addEventListener("click", handleClick)
+
+drawCardBtn.addEventListener("click", () => {
+          // `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(response => response.json())
+        .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+
+            cardsContainer.children[0].innerHTML = ` <img src=${data.cards[0].image} class="card" />`
+
+            cardsContainer.children[1].innerHTML = ` <img src=${data.cards[1].image} class="card" />`
+
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+
+            if (data.remaining === 0) {
+              drawCardBtn.disabled = true
+
+              if (computerScore > myScore) {
+                header.textContent = "The Computer won the game!"
+              }else if (myScore > computerScore) {
+                header.textContent = "You won the game!"
+              }else {
+                header.textContent = "It is a tie game!"
+              }
+            }
+
+        })
+})
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        computerScore++
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
+        return "Computer wins!"
+    }else if (card1ValueIndex < card2ValueIndex) {
+        myScore++
+        myScoreEl.textContent = `My score: ${myScore}`
+        return "You win!"
+    }else {
+        return "It is a War!"
+    }
+}
